55135c4f29a5c796402feeef74465754622d3689
EWERTON DA CRUZ PEREIRA <24.10113-1@maua.br>
2024-10-04 10:57:32 -0300

    até get oi

---
+++ index.js
@@ -0,0 +1,15 @@
+// console.log('hello, NodeJS')
+const express = require ('express')
+const app = express() //construindo uma aplicação express
+app.use(express.json ())
+
+//get url: http://localhost:3000/oi
+app.get ('/oi', (req, res) => {
+res. send('oi')
+
+
+})
+
+
+
+app.listen (3000, () => console.log("server up and running"))
